978bbb7267955e694736c1a06b90621bdb15591c
Pierre Schuvey <pierre.schuveye2n@gmail.com>
2017-10-25 15:51:55 +0200

    Fin de l'exercice 3 de la partie 3 du JS

---
+++ exo3.js
@@ -0,0 +1,12 @@
+function colorGreen()
+{
+    document.getElementById('texte').style.color = "green";
+}
+function colorRed()
+{
+    document.getElementById('texte').style.color = "red";
+}
+function colorBlue()
+{
+    document.getElementById('texte').style.color = "blue";
+}
